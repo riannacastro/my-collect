ca7f49f74d4aacfc315c3dce3829b5dde0cb3467
Rianna Castro <riannaxcastro@gmail.com>
2021-03-05 19:01:28 -1000

    commit tree 45deee17bd6dfacf4dc913d2eab56ab9493c9cf7

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+def my_collect(empty_array)
+    counter = 0
+    new_array = []
+    while empty_array.length > counter
+    new_array << yield(empty_array[counter])
+    counter += 1  
+end
 
+new_array
+
+end
 
